de60c65c4d5d8067ffa8fda1db04c78eeb1e1a2d
Adarsh kumar <adarshkumar26556@gmail.com>
2026-02-04 11:18:08 +0530

    Update API and Socket strings to use environment variables

--- src/API/postApi.js
+++ src/API/postApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const postAPI = axios.create({
-  baseURL: "http://localhost:5000/api/posts",
+  baseURL: `${import.meta.env.VITE_API_URL || "http://localhost:5000/api"}/posts`,
 });
 
 postAPI.interceptors.request.use((req) => {
